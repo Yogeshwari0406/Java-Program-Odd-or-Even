e83f7d545386c1f38fa7321512a957593379b631
Yogeshwari0406 <yogeshwari.cs.d@gmail.com>
2023-03-30 20:49:10 +0530

    Initial commit

---
+++ JavaPrograms/src/com/programs/OddOrEvenNumber.java
@@ -0,0 +1,14 @@
+package com.programs;
+
+public class OddOrEvenNumber {
+	public static void main(String[] args) {
+		int a = 877890087;
+		if(a%2==0)
+		{
+		System.out.println("Even Number");}
+		else
+			{
+			System.out.println("Odd Number");
+	}
+}
+}
---
+++ JavaPrograms/src/com/programs/PrintOddNumberEvenNumbers.java
@@ -0,0 +1,21 @@
+package com.programs;
+
+public class PrintOddNumberEvenNumbers {
+	//To Print Odd numbers upto 100
+	public static void main(String[] args) {
+	for (int i = 0; i <=100; i++) {
+		//if(i%2==1)
+		if(i%2!=0)
+		{
+			System.out.println(i);
+		}
+	}
+	//To Print Even numbers upto 100
+	for (int j = 0; j < 100; j++) {
+		//if(j%2==0)
+		if(j%2!=1)
+		{System.out.println(j);
+		
+	}
+}
+	}}
